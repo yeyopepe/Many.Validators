26ff5e198d5f164e3ff3e77537208ad5503fbce9
Sergio José Martínez Primiani <yeyopepe@hotmail.com>
2021-12-27 11:58:00 +0100

    Added: Benchmark results

--- Many.Validators.Benchmark/Benchmarks/NotNullOrEmptyIEnumerable/NotNullOrEmptyIEnumerableValidatorBenchmark.cs
+++ Many.Validators.Benchmark/Benchmarks/NotNullOrEmptyIEnumerable/NotNullOrEmptyIEnumerableValidatorBenchmark.cs
@@ -4,26 +4,26 @@ using System.Linq;
 
 namespace Many.Validators.Benchmark.Benchmarks.NotNullOrEmptyIEnumerable
 {
-	public class NotNullOrEmptyIEnumerableValidator_Null_IEnumerableBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<System.Collections.Generic.List<string>>
+	public class NotNullOrEmptyArrayValidator_Null_IEnumerableBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<System.Collections.Generic.List<string>>
     {
-        public NotNullOrEmptyIEnumerableValidator_Null_IEnumerableBenchmark() 
+        public NotNullOrEmptyArrayValidator_Null_IEnumerableBenchmark() 
             : base(argumentsToFail: null,
                    argumentsToSuccess: Utils.GetList(2))
         {
         }
     }
-    public class NotNullOrEmptyIEnumerableValidator_Empty_IEnumerableBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<System.Collections.Generic.List<string>>
+    public class NotNullOrEmptyArrayValidator_Empty_IEnumerableBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<System.Collections.Generic.List<string>>
     {
-        public NotNullOrEmptyIEnumerableValidator_Empty_IEnumerableBenchmark()
+        public NotNullOrEmptyArrayValidator_Empty_IEnumerableBenchmark()
             : base(argumentsToFail: new System.Collections.Generic.List<string>(),
                    argumentsToSuccess: Utils.GetList(2))
         {
         }
     }
 
-    public class NotNullOrEmptyIEnumerableValidator_Null_AnyMethodBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
+    public class NotNullOrEmptyArrayValidator_AnyMethodBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
     {
-        public NotNullOrEmptyIEnumerableValidator_Null_AnyMethodBenchmark()
+        public NotNullOrEmptyArrayValidator_AnyMethodBenchmark()
             : base(argumentsToFail: null,
                    argumentsToSuccess: Utils.GetArray(2),
                    withNoValidatorImplementationFunc: new Func<string[], string[]>((a) => WithNoValidatorImplementation(a)))
@@ -39,20 +39,10 @@ namespace Many.Validators.Benchmark.Benchmarks.NotNullOrEmptyIEnumerable
             return value;
         }
     }
-    public class NotNullOrEmptyIEnumerableValidator_Empty_AnyMethodBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
-    {
-        public NotNullOrEmptyIEnumerableValidator_Empty_AnyMethodBenchmark()
-            : base(argumentsToFail: new string[0],
-                   argumentsToSuccess: Utils.GetArray(2),
-                   withNoValidatorImplementationFunc: new Func<string[], string[]>((a) => NotNullOrEmptyIEnumerableValidator_Null_AnyMethodBenchmark.WithNoValidatorImplementation(a)))
-        {
-        }
 
-    }
-
-    public class NotNullOrEmptyIEnumerableValidator_Null_CountMethodBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
+    public class NotNullOrEmptyArrayValidator_CountMethodBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
     {
-        public NotNullOrEmptyIEnumerableValidator_Null_CountMethodBenchmark()
+        public NotNullOrEmptyArrayValidator_CountMethodBenchmark()
             : base(argumentsToFail: null,
                    argumentsToSuccess: Utils.GetArray(2),
                    withNoValidatorImplementationFunc: new Func<string[], string[]>((a) => WithNoValidatorImplementation(a)))
@@ -69,9 +59,9 @@ namespace Many.Validators.Benchmark.Benchmarks.NotNullOrEmptyIEnumerable
         }
     }
 
-    public class NotNullOrEmptyIEnumerableValidator_Null_LengthPropertyBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
+    public class NotNullOrEmptyArrayValidator_LengthPropertyBenchmark : NotNullOrEmptyIEnumerableValidatorBenchmarkBase<string[]>
     {
-        public NotNullOrEmptyIEnumerableValidator_Null_LengthPropertyBenchmark()
+        public NotNullOrEmptyArrayValidator_LengthPropertyBenchmark()
             : base(argumentsToFail: null,
                    argumentsToSuccess: Utils.GetArray(2),
                    withNoValidatorImplementationFunc: new Func<string[], string[]>((a) => WithNoValidatorImplementation(a)))
--- Many.Validators.Benchmark/Program.cs
+++ Many.Validators.Benchmark/Program.cs
@@ -17,12 +17,11 @@ namespace Many.Validators.Benchmark
             //var summary1 = BenchmarkRunner.Run<Benchmarks.NotNull.NotNullValidatorNullableClassBenchmark>();
             //var summary2 = BenchmarkRunner.Run<Benchmarks.NotNull.NotNullValidatorNullableStructBenchmark>();
             //var summary3 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyValidatorBenchmark>();
-            var summary4 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyIEnumerableValidator_Null_IEnumerableBenchmark>();
-            var summary41 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyIEnumerableValidator_Empty_IEnumerableBenchmark>();
-            var summary42 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyIEnumerableValidator_Null_AnyMethodBenchmark>();
-            var summary43 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyIEnumerableValidator_Empty_AnyMethodBenchmark>();
-            var summary44 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyIEnumerableValidator_Null_CountMethodBenchmark>();
-            var summary45 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyIEnumerableValidator_Null_LengthPropertyBenchmark>();
+            //var summary4 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyArrayValidator_Null_IEnumerableBenchmark>();
+            //var summary41 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyArrayValidator_Empty_IEnumerableBenchmark>();
+            var summary42 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyArrayValidator_AnyMethodBenchmark>();
+            var summary44 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyArrayValidator_CountMethodBenchmark>();
+            var summary45 = BenchmarkRunner.Run<Benchmarks.NotNullOrEmptyIEnumerable.NotNullOrEmptyArrayValidator_LengthPropertyBenchmark>();
 
             //var summary5 = BenchmarkRunner.Run<Benchmarks.Positive.PositiveValidatorIntBenchmark>();
             //var summary6 = BenchmarkRunner.Run<Benchmarks.Positive.PositiveValidatorNullableIntBenchmark>();
